09e774a657945f100564fadea441da0c1e816ae7
wayneb97 <w_l_b@outlook.com>
2025-07-22 19:33:06 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,21 @@
 import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "../types/supabase";
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "@/types/supabase";
 
-export const createClient = () =>
-  createBrowserClient<Database>(
+export function createClient() {
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+}
+
+export function createServerComponentClient() {
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies,
+    }
+  );
+}
