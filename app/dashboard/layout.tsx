647ee4d6443934a8d3030790005549ebc0b05eec
wayneb97 <w_l_b@outlook.com>
2025-07-22 17:57:52 +0100

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: 'Dashboard',
+};
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div>
+      {children}
+    </div>
+  );
 }
