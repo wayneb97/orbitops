c6fe8b0a5213f9a02140199d1690b96af89378eb
wayneb97 <w_l_b@outlook.com>
2025-07-22 17:42:42 +0100

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
-
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
