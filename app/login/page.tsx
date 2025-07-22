0201d958ae76ff4a667ca8b98833fa6d95d8489c
wayneb97 <w_l_b@outlook.com>
2025-07-22 18:41:08 +0100

    Add files via upload

---
+++ app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <div className="p-6">Login page content</div>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <main className="p-8 text-center">Welcome to OrbitOps!</main>;
+}
