e46f8787db558407dfd40bf55ebc07d925b8bcdf
wayneb97 <w_l_b@outlook.com>
2025-07-22 18:38:50 +0100

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// example middleware for auth (optional)
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
